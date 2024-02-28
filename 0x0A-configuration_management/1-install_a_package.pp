2fff7c66c1d5ce693e46f80495db7a1c634006cf
Annsrutto <annsrutto567@gmail.com>
2024-02-29 01:49:49 +0300

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-#Using Puppet, install flask from pip3
-package { 'flask':
-  ensure => '2.1.0',
+# Ensure the package 'python3-pip' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
