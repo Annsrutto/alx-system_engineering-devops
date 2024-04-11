b261eaa793490d0a4ce56a1e75314c5acc21c8f0
Annsrutto <annsrutto567@gmail.com>
2024-04-11 06:59:36 +0300

    number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-"""
+"""Queries the Reddit API and returns the number
+of subscribers for a given subreddit."""
 
 import requests
 from sys import argv
@@ -13,13 +12,14 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'codergirl'}
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
