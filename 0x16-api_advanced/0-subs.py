8ec2f4e071997038c8083db06c3da444399a4c1d
Annsrutto <annsrutto567@gmail.com>
2024-04-11 07:04:15 +0300

    number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'codergirl'}
+    headers = {'User-Agent': 'MyBot/0.1'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
